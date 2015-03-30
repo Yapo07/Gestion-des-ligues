ad58ff989c5c21bd8e5a34702717fda2363ab7f4
Zywodka <florile.maxime@gmail.com>
2015-03-30 22:40:21 +0200

    Avancement onglet GestionLigues

--- Personnel/src/applicationbureau/Compte.java
+++ Personnel/src/applicationbureau/Compte.java
@@ -7,43 +7,41 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class Compte extends JPanel{
+	JLabel lblNom;
+	JTextField txtNom;
+	JLabel lblPrenom;
+	JTextField txtPrenom;
+	JLabel lblEmail;
+	JTextField txtEmail;
+	JButton btnModifierMdp;
+	
 	public Compte(){
-		//Afficher les infos du compte root dans des textArea modifiable
-		//Un bouton modif MDP qui ouvre 2 msgBox, modif mdp, confirmation nouveau mdp
-		
 		setLayout(null);
 		
-		JLabel lblNom = new JLabel("Nom");
+		lblNom = new JLabel("Nom:");
 		lblNom.setBounds(29, 33, 46, 14);
-		
-		JTextField nom = new JTextField();
-		nom.setColumns(10);
-		nom.setBounds(141, 30, 86, 20);
-		
-		JLabel lblPrenom = new JLabel("Prenom");
+		txtNom = new JTextField();
+		txtNom.setColumns(10);
+		txtNom.setBounds(141, 30, 86, 20);
+		lblPrenom = new JLabel("Prenom:");
 		lblPrenom.setBounds(29, 76, 46, 14);
-		
-		JTextField prenom = new JTextField(20);
-		prenom.setColumns(10);
-		prenom.setBounds(141, 73, 86, 20);
-		
-		JLabel lblEmail = new JLabel("Email");
+		txtPrenom = new JTextField(20);
+		txtPrenom.setColumns(10);
+		txtPrenom.setBounds(141, 73, 86, 20);
+		lblEmail = new JLabel("Email:");
 		lblEmail.setBounds(29, 126, 46, 14);
-		
-		
-		JTextField email = new JTextField();
-		email.setColumns(10);
-		email.setBounds(141, 123, 86, 20);
-		
-		JButton btnModifierMdp = new JButton("Modifier MDP");
+		txtEmail = new JTextField();
+		txtEmail.setColumns(10);
+		txtEmail.setBounds(141, 123, 86, 20);
+		btnModifierMdp = new JButton("Modifier MDP");
 		btnModifierMdp.setBounds(84, 200, 110, 23);
 		
 		add(lblNom);
-		add(nom);
+		add(txtNom);
 		add(lblPrenom);
-		add(prenom);
+		add(txtPrenom);
 		add(lblEmail);
-		add(email);
+		add(txtEmail);
 		add(btnModifierMdp);
 	}
 }
