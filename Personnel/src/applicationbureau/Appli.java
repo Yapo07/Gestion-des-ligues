7a59110f04be87e12598ad0ca0d17dc0b7648db7
Zywodka <florile.maxime@gmail.com>
2015-03-31 11:28:07 +0200

    Commit Test

--- Personnel/src/applicationbureau/Appli.java
+++ Personnel/src/applicationbureau/Appli.java
@@ -9,6 +9,9 @@ public class Appli {
 
 	public static void main(String[] args) {
 		GestionPersonnel gp = personnel.GestionPersonnel.getGestionPersonnel();
+//		JDBC db = new JDBC();
+//		String req = "SELECT * FROM utilisateur";
+//		db.ExecReq(req);
 		SwingUtilities.invokeLater(new Runnable()
 		{
 					public void run()
