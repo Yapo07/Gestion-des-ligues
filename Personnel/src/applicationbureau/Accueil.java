6aab01a46f6c843a08dd7ecabd3b33d9de9aad27
Keylies <lemenct@gmail.com>
2015-03-31 11:37:29 +0200

    Ajout commentaire test

--- Personnel/src/applicationbureau/Accueil.java
+++ Personnel/src/applicationbureau/Accueil.java
@@ -5,6 +5,6 @@ import javax.swing.JPanel;
 public class Accueil extends JPanel{
 	public Accueil(){
 		
-		//Page d'accueil, contenu à définir		
+		//Page d'accueil, contenu à définir, une longue page	
 	}
 }
